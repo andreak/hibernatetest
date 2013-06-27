b281bd5277870884bfdc1c1cd8224aec443e507c
Andreas Joseph Krogh <andreak@officenet.no>
2013-06-27 21:33:52 +0200

    Added tests which fails in Hibernate in separate branch

--- src/test/java/no/officenet/test/hibernatetest/BlobTest.java
+++ src/test/java/no/officenet/test/hibernatetest/BlobTest.java
@@ -1,20 +1,15 @@
 package no.officenet.test.hibernatetest;
 
-import no.officenet.test.hibernatetest.model.AbstractEntity;
 import no.officenet.test.hibernatetest.model.Car;
 import no.officenet.test.hibernatetest.model.Company;
 import no.officenet.test.hibernatetest.model.FileRawData;
-import no.officenet.test.hibernatetest.model.Person;
 import no.officenet.test.hibernatetest.service.CarRepository;
 import no.officenet.test.hibernatetest.service.CompanyRepository;
 import no.officenet.test.hibernatetest.service.EntityRepository;
 import no.officenet.test.hibernatetest.service.PersonRepository;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.hibernate.LobHelper;
 import org.hibernate.Session;
-import org.springframework.aop.framework.Advised;
-import org.springframework.aop.support.AopUtils;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ConnectionCallback;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -24,13 +19,9 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.annotation.Resource;
@@ -41,20 +32,17 @@ import javax.persistence.PersistenceUnitUtil;
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Collections;
 
 @Test
 @ContextConfiguration(locations = {
 	"classpath*:spring/*-context.xml"
 })
-public class LazyLoadTest extends AbstractTestNGSpringContextTests {
+public class BlobTest extends AbstractTestNGSpringContextTests {
 
 	@Resource
 	PersonRepository personRepository;
@@ -84,7 +72,7 @@ public class LazyLoadTest extends AbstractTestNGSpringContextTests {
 		persistenceUnitUtil = entityManagerFactory.getPersistenceUnitUtil();
 	}
 
-	public void testCreateBlobFails() throws Exception {
+	public void testCreateBlobUsingLobHelperFails() throws Exception {
 		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 			@Override
 			protected void doInTransactionWithoutResult(TransactionStatus status) {
@@ -103,42 +91,71 @@ public class LazyLoadTest extends AbstractTestNGSpringContextTests {
 		});
 	}
 
-	public void testCreateBlobSuccess() throws Exception {
+	public void testCreateBlobSuccessButCommitFails() throws Exception {
 		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 			@Override
 			protected void doInTransactionWithoutResult(TransactionStatus status) {
-				try {
-					Blob b = new JdbcTemplate(dataSource).execute(new ConnectionCallback<Blob>() {
-						@Override
-						public Blob doInConnection(Connection con) throws SQLException, DataAccessException {
-							Blob broll = con.createBlob();
-							OutputStream outputStream = broll.setBinaryStream(1);
-							try {
-								FileUtils.copyFile(new File("/home/andreak/a.txt"), outputStream);
-								outputStream.flush();
-								outputStream.close();
-							} catch (Exception e) {
-								e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-							}
-							return broll;
+				companyRepository.save(new Company("Stuffaroo_" + System.currentTimeMillis()));
+				Blob b = new JdbcTemplate(dataSource).execute(new ConnectionCallback<Blob>() {
+					@Override
+					public Blob doInConnection(Connection con) throws SQLException, DataAccessException {
+						Blob b = con.createBlob();
+						OutputStream outputStream = b.setBinaryStream(1);
+						try {
+							FileUtils.copyFile(new File("/home/andreak/a.txt"), outputStream);
+//							FileUtils.copyFile(new File("/home/andreak/Downloads/quantal-desktop-amd64.iso"), outputStream);
+							outputStream.flush();
+							outputStream.close();
+						} catch (IOException e) {
+							e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
 						}
-					});
-					Car car = new Car("Wroooooooooooooooom!");
-					car.setData(new FileRawData(b));
-					carRepository.save(car);
-				} catch (Exception e) {
-					throw new RuntimeException(e.getMessage(), e);
-				}
+						return b;
+					}
+				});
+				Car car = new Car("Vamonda!");
+				car.setData(new FileRawData(b));
+				carRepository.save(car);
 			}
 		});
 	}
 
-	public void testReadBlob() throws Exception {
+	/**
+	 * Only difference between testCreateBlobFails is that this method doesn't save using companyRepository first
+	 * @throws Exception
+	 */
+	public void testCreateBlobFails() throws Exception {
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				Blob b = new JdbcTemplate(dataSource).execute(new ConnectionCallback<Blob>() {
+					@Override
+					public Blob doInConnection(Connection con) throws SQLException, DataAccessException {
+						Blob b = con.createBlob();
+						OutputStream outputStream = b.setBinaryStream(1);
+						try {
+							FileUtils.copyFile(new File("/home/andreak/a.txt"), outputStream);
+//							FileUtils.copyFile(new File("/home/andreak/Downloads/quantal-desktop-amd64.iso"), outputStream);
+							outputStream.flush();
+							outputStream.close();
+						} catch (IOException e) {
+							e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+						}
+						return b;
+					}
+				});
+				Car car = new Car("Vamonda!");
+				car.setData(new FileRawData(b));
+				carRepository.save(car);
+			}
+		});
+	}
+
+	public void testReadBlobHangsForever() throws Exception {
 		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 			@Override
 			protected void doInTransactionWithoutResult(TransactionStatus status) {
 				try {
-					final Car car = carRepository.retrieve(51l);
+					final Car car = carRepository.retrieve(177l);
 					FileUtils.copyInputStreamToFile(car.getData().getData().getBinaryStream(), new File("/home/andreak/hei-" + System.currentTimeMillis() + ".dmp"));
 				} catch (Exception e) {
 					throw new RuntimeException(e.getMessage(), e);
